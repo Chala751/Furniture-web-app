f0f45aec7b8032e996b6142ea418137805ca0a85
Chala Temesgen <ctemesgen85@gmail.com>
2025-05-09 09:06:46 -0300

    home page starting

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      <h1>wellcome to the home page</h1>
       <p className='text-3xl text-blue-500 flex justify-center items-center my-20'>This is from react,tailwind v-4 shadcn</p>
     </div>
   )
