5fc7d6d818032369a4bb738a81242d1fe381c336
Chala Temesgen <ctemesgen85@gmail.com>
2025-05-09 08:59:38 -0300

    first commit all setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-3xl text-blue-500 flex justify-center items-center my-20'>This is from react,tailwind v-4 shadcn</p>
+    </div>
+  )
+}
+
+export default App
